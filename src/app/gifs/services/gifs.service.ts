061f28a5c4ce856130551524f7dd3f441a076d0d
Jair <jairaceves56@gmail.com>
2023-08-25 13:41:07 -0600

    mantenemos si el registro existe entonces lo mandamos al inicio

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -5,15 +5,29 @@ export class GifsService {
     
     private _tagsHistory: string[] = []
     
-    
     constructor() { }
 
     get tagsHistory() {
         return [...this._tagsHistory]
     }
 
+    private organizedHistory( tag: string ){
+        tag = tag.toLocaleLowerCase()
+
+        if( this._tagsHistory.includes(tag) ){ // Si el TagHistory incluye el nuevo tag
+            this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag ) // Filtro si hay uno igual
+        }
+        this._tagsHistory.unshift( tag ) // Inserto al inicio
+        this._tagsHistory = this.tagsHistory.slice(0,10)
+
+    }
+
     searchTag( tag: string ):void{
-        this._tagsHistory.unshift( tag )  // Agregar el Tag
+
+        if( tag.length === 0) return
+        this.organizedHistory(tag)
+
+        // this._tagsHistory.unshift( tag )  // Agregar el Tag
         // console.log( this.tagsHistory );
     }
 }
