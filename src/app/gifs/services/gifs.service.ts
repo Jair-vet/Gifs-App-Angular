3bb07622f7d0b409cacf4b1e7f2a14d353f2e3ea
Jair <jairaceves56@gmail.com>
2023-08-28 10:34:36 -0600

    Obtenemos los gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,12 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
     
+    public gifList: Gif[] = []
+
     private _tagsHistory:    string[] = []
     private apiKey:          string = '9x5Taajs8y4mn8aunBYfY6ECoUi4JVa7'
     private serviceUrl:      string = 'https://api.giphy.com/v1/gifs';
@@ -36,9 +39,12 @@ export class GifsService {
             .set('limit', '10')
             .set('q', tag)
 
-        this.http.get(`${ this.serviceUrl }/search`, { params })
+        this.http.get<SearchResponse>(`${ this.serviceUrl }/search`, { params })
             .subscribe( resp => {
-                console.log(resp);
+
+                this.gifList = resp.data
+                console.log({ gifs: this.gifList});
+                
                 
             })
 
